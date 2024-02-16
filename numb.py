6207e2a39fef0d9bd8b6845f2533907571697ce1
HyeonuJeong <hyeonu4945@gmail.com>
2024-02-16 10:06:25 +0900

    filename

--- numb.py
+++ numb.py
@@ -1,11 +1,6 @@
 from collections.abc import Iterable
 
-
 def sum_even_numbers(numbers: Iterable[int]) -> int:
     """Given an iterable of integers,
     return the sum of all even numbers in the iterable."""
     return sum(num for num in numbers if num % 2 == 0)
-
-def test_sum_even_numbers(numbers: Iterable[int]) -> int:
-    
-    assert sum_even_numbers([2,4]) == 3
