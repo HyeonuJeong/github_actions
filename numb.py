fb3fc109efc5c1e5ad95e47d450072cd789bc720
HyeonuJeong <hyeonu4945@gmail.com>
2024-02-16 10:30:22 +0900

    linter test

--- numb.py
+++ numb.py
@@ -4,4 +4,5 @@ from collections.abc import Iterable
 def sum_even_numbers(numbers: Iterable[int]) -> int:
     """Given an iterable of integers,
     return the sum of all even numbers in the iterable."""
-    return sum(num for num in numbers if num % 2 == 0)
+    return sum(num for num in numbers 
+               if num % 2 == 0)
