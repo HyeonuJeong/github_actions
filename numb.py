af09ec37c4ce5efd6250f06a9cb193999e41ef15
HyeonuJeong <hyeonu4945@gmail.com>
2024-02-16 13:35:58 +0900

    not change

--- numb.py
+++ numb.py
@@ -1,6 +1,5 @@
 from collections.abc import Iterable
 
-
 def sum_even_numbers(numbers: Iterable[int]) -> int:
     """Given an iterable of integers,
     return the sum of all even numbers in the iterable."""
