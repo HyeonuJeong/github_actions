cf4471c7413d02ab3e42a2fea76f83256b9da23d
HyeonuJeong <hyeonu4945@gmail.com>
2024-04-29 16:53:23 +0900

    test actions

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ app = FastAPI()
 
 @app.get("/items/{item_id}")
 async def read_item(item_id):
+    print(f"Received request for item_id: {item_id}")
     return {"item_id": item_id}
 
 
--- test_example/test_numb.py
+++ test_example/test_numb.py
@@ -1,5 +1,5 @@
-import os
-import sys
+
+import sys,os
 
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
