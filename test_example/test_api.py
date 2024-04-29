fee43eec40840953ca7a3e7a0d4151de99b46fd0
HyeonuJeong <hyeonu4945@gmail.com>
2024-04-29 16:49:50 +0900

    fix change

--- test_example/test_api.py
+++ test_example/test_api.py
@@ -1,10 +1,14 @@
-import sys, os
+import os
+import sys
+
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
-from main import app
 from fastapi.testclient import TestClient
 
+from main import app
+
 client = TestClient(app)
+
 
 def test_read_item():
     response = client.get("/items/foo")
--- test_example/test_numb.py
+++ test_example/test_numb.py
@@ -1,5 +1,6 @@
+import os
+import sys
 
-import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
 from numb import sum_even_numbers
