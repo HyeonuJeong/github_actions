3cd7462034e831d05ce2e9b7b7f808afcdb013f2
HyeonuJeong <hyeonu4945@gmail.com>
2024-05-03 17:25:17 +0900

    clean

--- build.sh
+++ build.sh
@@ -1,12 +1,18 @@
+#!/bin/zsh
+
 pid=$(lsof -ti :7070)
+current_date=$(date "+%Y-%m-%d")
+current_time=$(date "+%H-%M-%S")
 
 kill -9 $pid
-
+sleep 3
 SCRIPT_DIR=$(dirname "$0")
 cd $SCRIPT_DIR
 
+
 git pull
-
+sleep 3
 git checkout $1
+sleep 3
+nohup python main.py > log_${current_date}_${current_time}.log 2>&1 &
 
-python main.py
--- main.py
+++ main.py
@@ -14,26 +14,16 @@ async def read_item(item_id):
 
 
 async def execute_build_script(version):
-    # 빌드 스크립트 실행
-    root = os.path.dirname(__file__)
-    build_file = os.path.join(root, "build.sh")
-
-    # 외부 명령 비동기 실행
+    build_file = os.path.join(os.path.dirname(__file__), "build.sh")
     process = await asyncio.create_subprocess_shell(f"sh {build_file} {version}")
     await process.wait()
-    return "build finish"
-
+    
 
 @app.get("/build/{version}")
 async def update_version(version):
-    print(f"Received request for version: {version}")
-
     asyncio.create_task(execute_build_script(version))
-
-    return "build finish"
+    return f"Received request for version: {version}"
 
 
 if __name__ == "__main__":
-    root = os.path.dirname(__file__)
-    print(root)
     uvicorn.run(app, host="0.0.0.0", port=7070)
